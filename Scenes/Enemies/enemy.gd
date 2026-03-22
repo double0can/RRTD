7858ef2ae0b09a33bac2dcd81efbe15fa4cd5565
double0can <46379734+double0can@users.noreply.github.com>
2026-03-22 23:35:04 +0000

    successfully implemented an enemy that follows the path
    implemented the script for path finding to an enemy which spawns in when the game scene is started.

---
+++ Scenes/Enemies/enemy.gd
@@ -0,0 +1,14 @@
+extends PathFollow2D
+
+@export var speed = 125
+#built in function to call and object to move 60 times a second
+func _physics_process(delta: float) -> void:
+	move(delta)
+
+#set the offset for the path
+func move(delta: float) -> void:
+	progress += speed * delta
+	
+	#checks if enemy has reached end of path. 0.0 at start, 1.0 at end
+	if progress_ratio >= 1.0:
+	queue_free()
