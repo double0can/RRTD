951f5d9224ae8924c3b56ef555c68311cf7f4d5f
double0can <46379734+double0can@users.noreply.github.com>
2026-03-23 00:21:59 +0000

    Implemented Game Over scene with buttons
    Added game over scene which displays when the player loses with buttons to restart, go to main menu, and to quit. Main menu button scripts were left as comments here as the main menu was not implemented at this point.

---
+++ Scenes/Game Over Scene/gameOverUI.gd
@@ -0,0 +1,32 @@
+extends CanvasLayer
+
+@onready var restartButton = $Control/VBoxContainer/RestartButton
+@onready var quitButton = $Control/VBoxContainer/QuitButton
+
+func _ready() -> void:
+	# Hide the menu when the game starts
+	visible = false
+	
+	restartButton.pressed.connect(_on_restart_pressed)
+	quitButton.pressed.connect(_on_quit_pressed)
+	
+	# listens for Game Over signal
+	GameManager.gameOver.connect(_on_game_over)
+
+func _on_game_over() -> void:
+	visible = true
+	# pauses the game on loss
+	get_tree().paused = true
+
+func _on_restart_pressed() -> void:
+	# Reset lives in GameManager
+	GameManager.lives = 20 
+	
+	#Unpauses the game
+	get_tree().paused = false
+	
+	#Reloades the current level
+	get_tree().reload_current_scene()
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
