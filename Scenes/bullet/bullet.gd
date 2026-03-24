854484dd4a5897b5093d75964303c291b1021c02
double0can <46379734+double0can@users.noreply.github.com>
2026-03-24 13:44:18 +0000

    fixed enemies not dying
    original error fixed here was a typo. I accidently called function takeDamage() in the enemies script takenDamage() instead. Therefore the bullet couldn't "find" the enemies to kill them.

--- Scenes/bullet/bullet.gd
+++ Scenes/bullet/bullet.gd
@@ -8,3 +8,18 @@ func _physics_process(delta: float) -> void:
 	global_position += transform.x * speed * delta
 
 func _on_area_entered(area: Node2D) -> void:
+	dealDamage(area)
+
+func dealDamage(target: Node2D) -> void:
+	if target.is_in_group("enemy"):
+		print("bullet hit")
+		
+		var enemyRoot = target.get_parent()
+		if enemyRoot.has_method("takeDamage"):
+			print("health script attached on parent")
+			enemyRoot.takeDamage(damage)
+			
+		else:
+			print("bullet hit but couldnt find takeDamage")
+			
+		queue_free()
